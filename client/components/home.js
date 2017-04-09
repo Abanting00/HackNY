495fe3a939ceca04e3bae81927173c72c7075f83
Chris Panican <Chris Panican>
2017-04-08 22:57:01 -0400

    added wrapper button with sign up

--- client/components/home.js
+++ client/components/home.js
@@ -1,20 +1,37 @@
 import React      from 'react';
+import Dashboard from '../components/dashboard';
+import Registration from '../components/registration'
+import ReactDOM from 'react-dom';
 import { Meteor } from 'meteor/meteor';
 
 export default class Home extends React.Component {
-    render() {
-          return (
-            <div className="row">
-              <div className="col-xs-6">
-                <img src=""/>
-              </div>
-              <div className="col-xs-6">
-                <h2>Welcome</h2>
-                <p>
+    showSignup() {
+        var element = <Registration/>;
+        ReactDOM.render(element, document.getElementById('signup'))
+    }
 
-                </p>
-              </div>
+    checkLogin() {
+        if (Meteor.loggingIn() || Meteor.user()) {
+            return (<Dashboard/>)
+        }
+        else {
+            return (
+                <div>
+                    <div>
+                        <h4>Picture goes here</h4>
+                    </div>
+                    <div id="signup">
+                        <input type="button" value="signup" onClick={this.showSignup.bind(this)}/>
+                    </div>
+                </div>
+            )
+        }
+    }
+    render() {
+        return (
+            <div className="row">
+                {this.checkLogin()}
             </div>
-          );
+        );
     }
 } // end class
